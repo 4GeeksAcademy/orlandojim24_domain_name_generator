08755dfa7980dd9c9bb02e89c4fe59956bfd0440
OrlandoJim24 <orlandojim2023@gmail.com>
2025-04-27 18:26:02 +0000

    Finished project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let pronouns = ['the', 'our'];
+  let adjetives = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let domains = [ '.com', '.net', '.us', '.io'];
+
+   for (let pronoun of pronouns ){
+    for (let adjetive of adjetives) {
+      for (let noun of nouns ) {
+        for (let domain of domains )
+          console.log(`${pronoun}${adjetive}${noun}${domain}`)
+      }
+    }
+   }
+}
