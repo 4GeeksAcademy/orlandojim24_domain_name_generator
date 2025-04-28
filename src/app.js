8ac82f8a715c9a73afdd214df778b517192e41d5
OrlandoJim24 <orlandojim2023@gmail.com>
2025-04-28 23:48:34 +0000

    coding guideline challenge

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
 
-  let pronouns = ['the', 'our'];
-  let adjetives = ['great', 'big'];
-  let nouns = ['jogger', 'racoon'];
-  let domains = [ '.com', '.net', '.us', '.io'];
+  let pronouns = ["the", "our"];
+  let adjetives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".net", ".us", ".io"];
 
-   for (let pronoun of pronouns ){
-    for (let adjetive of adjetives) {
-      for (let noun of nouns ) {
-        for (let domain of domains )
-          console.log(`${pronoun}${adjetive}${noun}${domain}`)
+  for (const pronoun of pronouns) {
+    for (const adjetive of adjetives) {
+      for (const noun of nouns) {
+        for (const domain of domains) {
+          console.log(`${pronoun}${adjetive}${noun}${domain}`); 
+        }
       }
     }
-   }
-}
+  }
+};
